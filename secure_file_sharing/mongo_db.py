0a32efb8c94160e576121f23ebc15209979df908
AJ <risliya2000@gmail.com>
2026-01-14 02:39:04 +0530

    Delete secure_file_sharing directory

--- secure_file_sharing/mongo_db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://127.0.0.1:27017")
-
-db = client["secure_file_sharing"]
-
-users_collection = db["users"]
-logs_collection = db["logs"]
